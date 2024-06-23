87c315edaecbc2b8b5ff4ebba9eb140b62f7ad34
Wai Hon Law <whhone@gmail.com>
2024-06-22 22:23:00 -0700

    update indent level from 4 to 2

--- content.js
+++ content.js
@@ -1,66 +1,66 @@
 const ALT_EVENT_KEYS = [
-    '[', ']', '=', '-',
-    '1', '2', '3', '4', '5', '6', '7', '8', '9'
+  '[', ']', '=', '-',
+  '1', '2', '3', '4', '5', '6', '7', '8', '9'
 ];
 
 const ALT_CTRL_EVENT_KEYS = [
-    '.', ','
+  '.', ','
 ]
 
 const CROS_FUNCTION_KEYS = [
-    "ZoomToggle",         // F3
-    "LaunchApplication1", // F4
-    "BrightnessDown",     // F5
-    "BrightnessUp",       // F6
-    "MediaPlayPause",     // F7
-    "AudioVolumeMute",    // F8
-    "AudioVolumeDown",    // F9
-    "AudioVolumeUp"       // F10
+  "ZoomToggle",         // F3
+  "LaunchApplication1", // F4
+  "BrightnessDown",     // F5
+  "BrightnessUp",       // F6
+  "MediaPlayPause",     // F7
+  "AudioVolumeMute",    // F8
+  "AudioVolumeDown",    // F9
+  "AudioVolumeUp"       // F10
 ];
 
 function isCrosAltKey(event) {
-    return event.altKey && !event.ctrlKey && ALT_EVENT_KEYS.includes(event.key);
+  return event.altKey && !event.ctrlKey && ALT_EVENT_KEYS.includes(event.key);
 }
 
 function isCrosAltCtrlKey(event) {
-    return event.altKey && event.ctrlKey && ALT_CTRL_EVENT_KEYS.includes(event.key)
+  return event.altKey && event.ctrlKey && ALT_CTRL_EVENT_KEYS.includes(event.key)
 }
 
 function isCrosFuncKey(event) {
-    return CROS_FUNCTION_KEYS.includes(event.key);
+  return CROS_FUNCTION_KEYS.includes(event.key);
 }
 
 function isDevelopModeKey(event) {
-    return event.ctrlKey && event.shiftKey && event.key === "I";
+  return event.ctrlKey && event.shiftKey && event.key === "I";
 }
 
 console.log("'PWA on Chrome OS Integration' extension is running");
 
 function isFullscreen() {
-    return document.fullscreenElement != null
+  return document.fullscreenElement != null
 }
 
 // Prevent CRD / PWA from capturing system keys
 document.addEventListener('keydown', (event) => {
-    if (isCrosFuncKey(event)
-        || (!isFullscreen() && isCrosAltKey(event))
-        || isCrosAltCtrlKey(event)
-        || isDevelopModeKey(event)) {
-        event.stopPropagation();
-    }
-    // console.log(event);
+  if (isCrosFuncKey(event)
+      || (!isFullscreen() && isCrosAltKey(event))
+      || isCrosAltCtrlKey(event)
+      || isDevelopModeKey(event)) {
+    event.stopPropagation();
+  }
+  // console.log(event);
 }, true);
 
 function setMetaTag(name, content) {
-    let metaTag = document.querySelector(`meta[name="${name}"]`);
-    if (!metaTag) {
-        // Create a new meta tag if it doesn't exist
-        metaTag = document.createElement('meta');
-        metaTag.name = name;
-        document.head.appendChild(metaTag);
-    }
+  let metaTag = document.querySelector(`meta[name="${name}"]`);
+  if (!metaTag) {
+    // Create a new meta tag if it doesn't exist
+    metaTag = document.createElement('meta');
+    metaTag.name = name;
+    document.head.appendChild(metaTag);
+  }
 
-    metaTag.content = content;
+  metaTag.content = content;
 }
 
 // Prefer darker title bar even in light theme.
